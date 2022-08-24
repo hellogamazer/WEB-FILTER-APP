1e4407a97cf51514bdab549c2d91a626982a7f9a
hellogamazer <86831212+hellogamazer@users.noreply.github.com>
2022-08-24 16:00:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+  clown_nose = loadImage('https://i.postimg.cc/3wv45LNS/mustache-student-math-favorite-for-friday-the-the-1.png');
+}
+
+function setup() {
+canvas = createCanvas(300,300);
+canvas.center();
+video =createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log ('poseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+if(results.length > 0 ) 
+  {
+ console.log(results);
+noseX = results[0].pose.nose.x;
+noseY = results[0].pose.nose.y;
+console.log("nose x = " + results[0].pose.nose.x);
+console.log("nose y = " + results[0].pose.nose.y);
+  }
+}
+
+function draw() {
+image(video, 0, 0, 300, 300);
+image(clown_nose, noseX-15,noseY-10, 40, 45);
+}
+
+function take_snapshot(){
+save('myFilterImage.png');
+}
